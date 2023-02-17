66faf7eb2874f807ff07ad42adbd53af45780064
Diego Queiroz <diegiwg@gmail.com>
2023-02-16 22:33:56 -0300

    1

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
 const app = document.getElementById("app");
 
+function speak(msg) {
+    const speaker = new SpeechSynthesisUtterance();
+    speaker.text = msg;
+    window.speechSynthesis.speak(speaker);
+}
+
 function myFunction(e) {
     if (e.key == "J" && e.shiftKey) {
-        alert(document.getElementById("jogador").textContent);
+        const j = document.getElementById("jogador").textContent;
+        speak(j);
     }
 }
 
