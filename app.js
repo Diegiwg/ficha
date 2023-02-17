3358b8f3faa790ea028a1444a1423e7f0acbc5c9
Diego Queiroz <diegiwg@gmail.com>
2023-02-16 22:54:16 -0300

    1

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
+function random(max) {
+    return Math.floor(Math.random() * max);
+}
+
 const app = document.getElementById("app");
 
 function speak(msg) {
@@ -17,6 +21,10 @@ document.addEventListener("keypress", myFunction);
 
 const ficha = {
     jogador: document.getElementById("jogador"),
+    vida_total: document.getElementById("vida-total"),
+    vitalidade: document.getElementById("vitalidade"),
 };
 
-ficha.jogador.textContent = `Jogador: ${Math.floor(Math.random() * 1000)}`;
+ficha.jogador.textContent = random(1000);
+ficha.vitalidade.textContent = random(10);
+ficha.vida_total.textContent = 100 + ficha.vitalidade.textContent * 10;
